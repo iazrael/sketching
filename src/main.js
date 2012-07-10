b096ff35d954341348c80c0cf8f8018065cbed14
azrael <azrael@imatlas.com>
2012-07-10 20:06:31 +0800

    alpha 版完成
    Signed-off-by: azrael <azrael@imatlas.com>

--- src/main.js
+++ src/main.js
@@ -54,76 +54,96 @@
      * @param  {Array} pixes  pix array
      * @param  {Number} width 图片的宽度
      * @param  {Number} height 图片的高度
-     * @param  {Number} radius 取样区域半径, 可选, 默认为 3.0
+     * @param  {Number} radius 取样区域半径, 正数, 可选, 默认为 3.0
+     * @param  {Number} sigma 标准方差, 可选, 默认取值为 radius / 3
      * @return {Array}
      */
-
-    function gaussBlur(pixes, width, height, radius) {
+    function gaussBlur(pixes, width, height, radius, sigma) {
         var gaussMatrix = [],
             gaussSum = 0,
-            diamet,
-            sigma,
-            sigma2,
-            x, y, m, n,
-            xx, yy,
+            x, y,
             r, g, b, a,
-            i, j, len;
+            i, j, k, len;
 
-        radius = radius || 3;
-        diamet = radius * 2 + 1;// 采样区域直径
-        sigma = radius / 3;
-        sigma2 = 2 * sigma * sigma;
-
+        radius = Math.floor(radius) || 3;
+        sigma = sigma || radius / 3;
+        
+        a = 1 / (Math.sqrt(2 * Math.PI) * sigma);
+        b = -1 / (2 * sigma * sigma);
         //生成高斯矩阵
-        for (i = 0, y = -radius; y <= radius; y++) {
-            for(x = -radius; x <= radius; x++, i++){
-                a = Math.exp(-(x * x + y * y) / sigma2);
-                gaussMatrix[i] = a;
-                gaussSum += a;
-            }
+        for (i = 0, x = -radius; x <= radius; x++, i++){
+            g = a * Math.exp(b * x * x);
+            gaussMatrix[i] = g;
+            gaussSum += g;
+        
         }
-        // console.log(JSON.stringify(gaussMatrix));
         //归一化, 保证高斯矩阵的值在[0,1]之间
         for (i = 0, len = gaussMatrix.length; i < len; i++) {
             gaussMatrix[i] /= gaussSum;
         }
-        // console.log(JSON.stringify(gaussMatrix));
         //x 方向一维高斯运算
         for (y = 0; y < height; y++) {
             for (x = 0; x < width; x++) {
                 r = g = b = a = 0;
-                j = 0;
-                // j = (y * width + x) * 4;
-                for(m = -radius; m <= radius; m++){
-                    yy = y + m;
-                    if(yy >= 0 && yy < height){
-                        for(n = -radius; n <= radius; n++){
-                            xx = x + n;
-                            if(x >= 0 && x < width){
-                                i = (height - yy - 1) * width + xx;
-                                r += pixes[i] * gaussMatrix[j];
-                                g += pixes[i + 1] * gaussMatrix[j];
-                                b += pixes[i + 2] * gaussMatrix[j];
-                            }
-                            j++;
-                        }
-                    }else{
-                        j += diamet;
+                for(j = -radius; j <= radius; j++){
+                    k = x + j;
+                    if(k >= 0 && k < width){//确保 k 没超出 x 的范围
+                        //r,g,b,a 四个一组
+                        i = (y * width + k) * 4;
+                        r += pixes[i] * gaussMatrix[j + radius];
+                        g += pixes[i + 1] * gaussMatrix[j + radius];
+                        b += pixes[i + 2] * gaussMatrix[j + radius];
+                        // a += pixes[i + 3] * gaussMatrix[j];
                     }
                 }
-                i = (height - y - 1) * width + x;
+                i = (y * width + x) * 4;
                 pixes[i] = r;
                 pixes[i + 1] = g;
                 pixes[i + 2] = b;
-                // pixes[i + 3] = a / gaussSum;
-                
+                // pixes[i + 3] = a ;
+            }
+        }
+        //y 方向一维高斯运算
+        for (x = 0; x < width; x++) {
+            for (y = 0; y < height; y++) {
+                r = g = b = a = 0;
+                for(j = -radius; j <= radius; j++){
+                    k = y + j;
+                    if(k >= 0 && k < height){//确保 k 没超出 y 的范围
+                        i = (k * width + x) * 4;
+                        r += pixes[i] * gaussMatrix[j + radius];
+                        g += pixes[i + 1] * gaussMatrix[j + radius];
+                        b += pixes[i + 2] * gaussMatrix[j + radius];
+                        // a += pixes[i + 3] * gaussMatrix[j];
+                    }
+                }
+                i = (y * width + x) * 4;
+                pixes[i] = r;
+                pixes[i + 1] = g;
+                pixes[i + 2] = b;
+                // pixes[i + 3] = a ;
             }
         }
         //end
         return pixes;
     }
 
-    function testCase(){
+    /************************* testCase ***************************/
+    function $(id){
+        return document.getElementById(id);
+    }
+    testDiscolor = function(){
+        var width = 2, height = 2,
+            pixes = [];
+        for(var i = 0, len = width * height * 4; i < len; i++){
+            pixes[i] = Math.round(255 * Math.random());
+        }
+        console.log(JSON.stringify(pixes));
+        discolor(pixes);
+        console.log(pixes);
+    }
+
+    testCaseGaussBlur = function(){
         var width = 5, height = 5,
             pixes = [];
         for(var i = 0, len = width * height * 4; i < len; i++){
@@ -134,6 +154,78 @@
         console.log(pixes);
     }
 
+    testCase = function (){
+        var source = $('source');
+        var ctx = source.getContext('2d');
+        var target = $('target');
+        var ctx2 = target.getContext('2d');
+        var tools = $('tools');
+
+        ctx.font= '30px sans-serif';
+        ctx.fillText('Drop a picture to here', 80, 100);
+
+        ctx2.font= '30px sans-serif';
+        ctx2.fillText('You will see the result in here', 20, 100);
+
+        source.addEventListener('drop', function(e){
+            e.preventDefault();
+            var file = e.dataTransfer.files[0];
+            var reader = new FileReader();
+            reader.onload = function(e){
+                var img = new Image();
+                img.onload = function(){
+                    ctx.drawImage(img, 0 ,0);
+                }
+                img.src = e.target.result;
+            }
+            reader.onerror = function(e){
+                alert(e.target.error.code);
+            }
+            reader.readAsDataURL(file);
+        });
+        tools.addEventListener('click', function(e){
+            if(e.target.tagName === 'INPUT'){
+                var value = e.target.value;
+                var imgData = ctx.getImageData(0, 0, source.width, source.height);
+                switch(value){
+                    case 'discolor':
+                        discolor(imgData.data);
+                        ctx2.putImageData(imgData, 0, 0);
+                        break;
+                    case 'invert':
+                        invert(imgData.data);
+                        ctx2.putImageData(imgData, 0, 0);
+                        break;
+                    case 'gaussBlur':
+                        gaussBlur(imgData.data, imgData.width, imgData.height);
+                        ctx2.putImageData(imgData, 0, 0);
+                        break;
+                    case 'dodgeColor':
+                        // dodgeColor(imgData.data, imgData2.data);
+                        // ctx2.putImageData(imgData, 0, 0);
+                        break;
+                    case 'sketch':
+                        imgData2 = ctx.getImageData(0, 0, source.width, source.height);
+                        discolor(imgData.data);
+                        discolor(imgData2.data);
+                       
+                        invert(imgData2.data);
+                        
+                        gaussBlur(imgData2.data, imgData2.width, imgData2.height, 5, 1);
+                        
+                        dodgeColor(imgData.data, imgData2.data);
+                        ctx2.putImageData(imgData, 0, 0);
+
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
     testCase();
+
 
 })();
